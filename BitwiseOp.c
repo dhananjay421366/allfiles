1dc46c99cda209ced960e5078294f803f1c93bc4
dhananjay421366 <nimbalkardhananjay349@gmail.com>
2024-09-25 07:21:40 +0530

    first commit

---
+++ BitwiseOp.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    int no = 10;
+    printf("The left Shift :%d\n",no<<2);
+    printf("The right Shiift :%d\n",no>>2);
+    return 0 ;
+}
---
+++ Enum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+enum days {Sunday,Monday,Tuesday,Wednesday};
+cout<<Sunday<<"\n";
+cout<<Monday<<"\n";
+enum Months {jan= 10,fub = 20,mar};
+cout<<jan<<"\n";
+cout<<fub<<"\n";
+cout<<mar
+<<"\n";
+    return 0;
+}
